10e4c59948e0539380d6119a6cc0ef5980582563
Rahul <rahul222366@gmail.com>
2019-05-24 10:51:17 +0530

    App Uploading

---
+++ app/src/main/java/com/happykrafts/jobschedular/ExampleJobService.java
@@ -0,0 +1,48 @@
+package com.happykrafts.jobschedular;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.util.Log;
+
+public class ExampleJobService extends JobService {
+
+    private static final String TAG = "ExampleJobService";
+    private boolean jobCancelled = false;
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+
+        Log.d(TAG,"Job Started");
+        doworkBackGround(params);
+        return true;
+    }
+
+    private void doworkBackGround(JobParameters params) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for(int i = 0;i>-1;i++){
+                    Log.d(TAG,"Run "+i);  //Infinite Loop
+                    if(jobCancelled){
+                        return;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+
+        Log.d(TAG,"Job cancelled before Completion");
+        jobCancelled = true;
+        return false;
+    }
+}
---
+++ app/src/main/java/com/happykrafts/jobschedular/MainActivity.java
@@ -0,0 +1,43 @@
+package com.happykrafts.jobschedular;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startJob(View view) {
+        ComponentName componentName = new ComponentName(this,ExampleJobService.class);
+        JobInfo info = new JobInfo.Builder(123,componentName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15*60*1000).build();
+
+        JobScheduler scheduler = (JobScheduler)getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode =scheduler.schedule(info);
+
+        if(resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d("Main Activity","Job scheduled");
+        }
+        else{
+            Log.d("Main Activity","Job Scheduling failed");
+        }
+    }
+
+    public void stopJob(View view) {
+        JobScheduler scheduler = (JobScheduler)getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d("Main Activity","Job scheduler cancelled ");
+    }
+}
